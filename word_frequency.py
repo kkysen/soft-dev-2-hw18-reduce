6cf2dc9d321e637776a1ed9eb02e4a1f674dfd13
Khyber <kkysen@gmail.com>
2018-04-30 21:10:04 -0400

    added alternate word group methods

--- word_frequency.py
+++ word_frequency.py
@@ -8,23 +8,34 @@ from __future__ import print_function
 import re
 from collections import Counter
 
+from typing import List
+
 
 class WordFrequencies(object):
     
     def __init__(self, path="Dante's Inferno.txt"):
-        self.text = open(path).read()
-        self.words = re.split("//S+", self.text)
-        self.word_frequencies = Counter(self.words)  # type: Counter
-        self.num_words = len(self.words)
+        # type: (str) -> None
+        self.text = open(path).read()  # type: str
+        # TODO check regex
+        self.words = re.split("//S+", self.text)  # type: List[str]
+        self.word_frequencies = Counter(self.words)  # type: Counter[str]
+        self.num_words = len(self.words)  # type: int
     
     def most_common_word(self):
-        return max(*self.word_frequencies.viewitems(), key=lambda item: item[1])
+        # type: () -> str
+        return max(*self.word_frequencies.viewitems(), key=lambda item: item[1])[0]
     
     def word_frequency(self, word):
+        # type: (str) -> int
         return self.word_frequencies[word]
     
     def phrase_frequency(self, phrase):
+        # type: (str) -> int
         return len((" " + self.text + " ").split(phrase)) - 1
+    
+    def words_frequency(self, words):
+        # type: (List[str]) -> int
+        return sum(self.word_frequencies[word] for word in words)
 
 
 def test():
